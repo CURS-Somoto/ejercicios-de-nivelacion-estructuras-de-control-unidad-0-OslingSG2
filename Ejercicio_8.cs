2d8e6a7715fd0386496bc5bb3914f655f741991e
OslingSG2 <147261945+OslingSG2@users.noreply.github.com>
2023-10-12 19:22:31 -0600

    Update Ejercicio_8.cs

--- Ejercicio_8.cs
+++ Ejercicio_8.cs
@@ -20,3 +20,32 @@ la siguiente secuencia de patrones:
 *****
 
 */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese numero");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
