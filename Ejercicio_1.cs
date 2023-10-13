ac46327cd0654539b1d226e89a9b976c167e53d1
OslingSG2 <147261945+OslingSG2@users.noreply.github.com>
2023-10-12 18:59:49 -0600

    Update Ejercicio_1.cs
    Entrega de trabajo N° 1

--- Ejercicio_1.cs
+++ Ejercicio_1.cs
@@ -3,3 +3,30 @@
 
 Escribe un programa
 que muestre los números pares del 2 al 20. */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("identificar los numeros pares del 2 al 20");
+            for (int i = 2; i <= 20; i = i + 2)  {
+
+                Console.WriteLine(i);
+            }
+            Console.Read();
+
+        }
+            
+                 
+    }
+}
+
