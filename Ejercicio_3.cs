6cbd5861af0f102667dfe15cd8c9bf4bff8ba9f0
OslingSG2 <147261945+OslingSG2@users.noreply.github.com>
2023-10-12 19:16:44 -0600

    Update Ejercicio_3.cs
    Entrega de ejercicios

--- Ejercicio_3.cs
+++ Ejercicio_3.cs
@@ -3,3 +3,57 @@
 
 Escribe un
 programa que determine si un número dado es primo o no.*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limite;
+            int aux;
+            string numerosprimos = "2";
+            int numEvaluar;
+            int ctrlNoEsprimo;
+
+            Console.WriteLine("Favor ingresar el limite hasta donde se calcularan los numeros primos");
+            limite = Convert.ToInt16( Console.ReadLine());
+
+            for (int i = 3; i <= limite; i++)
+            {
+                numEvaluar = i;
+                aux = 2;
+                ctrlNoEsprimo = 0;
+                while (aux < numEvaluar && ctrlNoEsprimo == 0)
+                {
+
+                    int operacion = numEvaluar % aux;
+                    if (operacion != 0)
+                    {
+                        aux += 1;
+                        //aux++
+                    }
+                    else
+                    {
+                        ctrlNoEsprimo = 1;
+                    }
+
+                }
+                if (ctrlNoEsprimo <= 0)
+                {
+                    numerosprimos = numerosprimos + "," + numEvaluar;
+                }
+            }
+            Console.WriteLine("Los numeros primos encontrados son: " + numerosprimos);
+            Console.ReadLine();
+
+        }
+    }
+}
