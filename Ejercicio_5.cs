eb5c4b31983ab43b4c4f7fedb37747ea5edf3b72
OslingSG2 <147261945+OslingSG2@users.noreply.github.com>
2023-10-12 19:19:19 -0600

    Update Ejercicio_5.cs

--- Ejercicio_5.cs
+++ Ejercicio_5.cs
@@ -3,3 +3,34 @@
 
 Escribe un programa en C# que determine si
 un número entero dado es positivo, negativo o cero.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrse numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            if(numero == 0)
+            {
+                Console.Write("El numero" + numero + "es un numero cero");
+            }
+            else if(numero > 0)
+            {
+                Console.Write("el numero" + numero + "es un numero positivo.");
+            }
+            else
+            {
+                Console.Write("el numero" + numero + "es un numero negativo.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
